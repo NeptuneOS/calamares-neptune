b0d6a1d558a41e968bb44471e6258c3311168f70
Leszek Lesner <leszek.lesner@web.de>
2018-02-09 18:16:13 +0100

    Also set correct policykit rule for disable/enable sudo

--- usr/lib/calamares/cala_disable_sudo.sh
+++ usr/lib/calamares/cala_disable_sudo.sh
@@ -5,4 +5,4 @@
 #
 kwriteconfig --file /usr/share/neptune-base/profiles/kde-profile/share/config/kdesurc --group super-user-command --key super-user-command su
 kwriteconfig5 --file /usr/share/neptune-base/profiles/plasma5-profile/kdesurc --group super-user-command --key super-user-command su 
- 
+sed -i "s/sudo/0/g" /etc/polkit-1/localauthority.conf.d/51-debian-sudo.conf 
--- usr/lib/calamares/cala_enable_sudo.sh
+++ usr/lib/calamares/cala_enable_sudo.sh
@@ -5,3 +5,4 @@
 #
 kwriteconfig --file /usr/share/neptune-base/profiles/kde-profile/share/config/kdesurc --group super-user-command --key super-user-command sudo
 kwriteconfig5 --file /usr/share/neptune-base/profiles/plasma5-profile/kdesurc --group super-user-command --key super-user-command sudo 
+sed -i "s/^AdminIdentities=.*\$/AdminIdentities=unix-group:sudo/" /etc/polkit-1/localauthority.conf.d/51-debian-sudo.conf
